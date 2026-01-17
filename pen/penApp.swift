67d03f528128c619a2b3987382cbabd25cdd6b5b
cwng2022 <toukinxi@gmail.com>
2026-01-17 21:15:04 +0800

    Initial Commit

---
+++ pen/penApp.swift
@@ -0,0 +1,17 @@
+//
+//  penApp.swift
+//  pen
+//
+//  Created by 陶建希 on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct penApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
